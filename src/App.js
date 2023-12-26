c56610762eaf252d46356593a995f0a537021f89
Prabu-2001 <asprabunkl@gmail.com>
2023-12-26 20:11:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Layout from "./PROJECT/Layout";
-import Home from "./PROJECT/Home";
-import Productlayout from "./PROJECT/Product_Layout";
-import Mens from "./PROJECT/Mens";
-import Womens from "./PROJECT/Womens";
-import Kids from "./PROJECT/Kids";
-import Add_products from "./PROJECT/Add_product";
-import User from "./PROJECT/User";
-import "./index.css";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import Layout from "./PROJECT/Layout";
+// import Home from "./PROJECT/Home";
+// import Productlayout from "./PROJECT/Product_Layout";
+// import Mens from "./PROJECT/Mens";
+// import Womens from "./PROJECT/Womens";
+// import Kids from "./PROJECT/Kids";
+// import Add_products from "./PROJECT/Add_product";
+// import User from "./PROJECT/User";
+// import "./index.css";
 
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+// import { BrowserRouter,Routes,Route } from "react-router-dom";
 
-function App()
-{
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout/>}>
-                    <Route index element={<Home/>}/>
-                    <Route path="Product_Layout" element={<Productlayout/>}>
-                        <Route index element={<Mens/>}/>
-                        <Route path="Product_Layout/Womens" element={<Womens/>}/>
-                        <Route path="Product_Layout/Kids" element={<Kids/>}/> 
-                    </Route>
-                    <Route path="Add_product" element={<Add_products/>}/>
-                    <Route path="User" element={<User/>}/>
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
-}
-export default App;
+// function App()
+// {
+//     return(
+//         <BrowserRouter>
+//             <Routes>
+//                 <Route path="/" element={<Layout/>}>
+//                     <Route index element={<Home/>}/>
+//                     <Route path="Product_Layout" element={<Productlayout/>}>
+//                         <Route index element={<Mens/>}/>
+//                         <Route path="Product_Layout/Womens" element={<Womens/>}/>
+//                         <Route path="Product_Layout/Kids" element={<Kids/>}/> 
+//                     </Route>
+//                     <Route path="Add_product" element={<Add_products/>}/>
+//                     <Route path="User" element={<User/>}/>
+//                 </Route>
+//             </Routes>
+//         </BrowserRouter>
+//     )
+// }
+// export default App;
